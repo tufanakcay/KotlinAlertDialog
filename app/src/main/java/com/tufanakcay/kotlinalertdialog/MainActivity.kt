74a26e1799efa7d908da6a94575271c67810c011
Tufan AKÇAY <132299647+tufanakcay@users.noreply.github.com>
2023-05-20 17:47:47 +0300

    Update

--- app/src/main/java/com/tufanakcay/kotlinalertdialog/MainActivity.kt
+++ app/src/main/java/com/tufanakcay/kotlinalertdialog/MainActivity.kt
@@ -2,6 +2,9 @@ package com.tufanakcay.kotlinalertdialog
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,7 +13,46 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //Context
+
+        //Activity Context -> this
+        //App Context -> applicationContext
+
+
+        Toast.makeText(this@MainActivity,"Welcome",Toast.LENGTH_LONG).show()
+
+
+        /*
+        button.setOnClickListener(object:View.OnClickListener {
+            override fun onClick(v: View?) {
+                Toast.makeText(this@MainActivity,"test2",Toast.LENGTH_LONG).show()
+            }
+        })
+
+         */
+
 
     }
 
+    fun save(view : View) {
+
+        val alert = AlertDialog.Builder(this@MainActivity)
+        alert.setTitle("Save")
+        alert.setMessage("Are You Sure?")
+
+        alert.setPositiveButton("Yes") {dialog, which ->
+
+            //OnClick Listener
+            Toast.makeText(this,"Saved",Toast.LENGTH_LONG).show()
+        }
+        alert.setNegativeButton("No") {dialog, which ->
+
+            //OnClick Listener
+            Toast.makeText(applicationContext,"Not Saved",Toast.LENGTH_LONG).show()
+
+        }
+
+        alert.show()
+
+    }
 }
